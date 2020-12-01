90547d5d9e4c63e3bf54f7c2384976f211aa1532
koussay bilel <koussaybilel02@gmail.com>
2020-12-01 14:44:13 +0100

    controles

---
+++ src/main/java/com/sip/ams/controllers/CommandeController.java
@@ -0,0 +1,26 @@
+package com.sip.ams.controllers;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.sip.ams.entities.Book;
+
+
+import com.sip.ams.repository.BookRepository;
+
+public class CommandeController {
+	
+	
+
+}
---
+++ src/main/java/com/sip/ams/entities/Admin.java
@@ -0,0 +1,5 @@
+package com.sip.ams.entities;
+
+public class Admin {
+
+}
