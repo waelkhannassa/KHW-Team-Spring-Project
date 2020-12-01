77709572a35ae0f6ec78e39a49ef254908a2933b
koussay bilel <koussaybilel02@gmail.com>
2020-12-01 21:04:24 +0100

    testtt

--- src/main/java/com/sip/ams/entities/Admin.java
+++ src/main/java/com/sip/ams/entities/Admin.java
@@ -1,46 +1,34 @@
 package com.sip.ams.entities;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> testtt
 import javax.persistence.Entity;
 
 @Entity 
 public class Admin extends User  {
 
-<<<<<<< HEAD
-=======
 
 
 
->>>>>>> testtt
+
   int id ;
   int s ;
   
   
-  Admin() {}
+ 
 
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> testtt
 public Admin(int id) {
 	super();
 	this.id = id;
 }
-<<<<<<< HEAD
-
-  
-}
-=======
 
 }
 
 
+
+
   
 
->>>>>>> testtt
+
 
