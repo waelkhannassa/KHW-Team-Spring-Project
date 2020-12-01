90a62e80ce7f202355128da82c5da43168520d25
koussay bilel <koussaybilel02@gmail.com>
2020-12-01 16:57:46 +0100

    commandecontroller

--- src/main/java/com/sip/ams/controllers/CommandeController.java
+++ src/main/java/com/sip/ams/controllers/CommandeController.java
@@ -15,11 +15,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.sip.ams.entities.Book;
-
-
+import com.sip.ams.entities.Commande;
+import com.sip.ams.repository.CommandeRepository;
 import com.sip.ams.repository.BookRepository;
 
 public class CommandeController {
+	
+	private CommandeRepository commandeRepository;
+
+	private BookRepository bookrepository;
+
+	
+	@Autowired
+	public CommandeController(CommandeRepository commandeRepository,BookRepository bookrepository) {
+		this.commandeRepository = commandeRepository;
+		
+		this.bookrepository = bookrepository;
+	}
+	
+	
+	@GetMapping("list")
+	@ResponseBody
+	public String listorders(Model model) {
+		List<Commande> com = (List<Commande>) commandeRepository.findAll();
+		if (com.size() == 0)
+			com = null;
+		model.addAttribute("commande", com);
+		return "aka";
+		
+		
+	}
+	
 	
 	
 
